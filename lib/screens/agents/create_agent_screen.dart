5e65780e0ed5bb48ef6a3c6fdcc05ca97511c983
Willis Ivali <itsivali@outlook.com>
2025-02-02 20:58:51 +0300

    Remove unused imports in create_agent_screen.dart for cleaner code

--- lib/screens/agents/create_agent_screen.dart
+++ lib/screens/agents/create_agent_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import '../../models/agent.dart';
 import '../../providers/agent_provider.dart';
-import '../../utils/validators.dart';
-import '../../widgets/loading_overlay.dart';
 
 class CreateAgentScreen extends StatelessWidget {
   const CreateAgentScreen({super.key});
